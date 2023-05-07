1c16810f9b27a0e3d4f6f9a1873a4e193c0a5a59
luonghieu911 <hieulx1st@gmail.com>
2023-05-08 06:22:20 +0700

    Khởi tạo dự án

---
+++ QL_LopHocPhan/QL_LopHocPhan/Controllers/SinhVienController.cs
@@ -0,0 +1,28 @@
+﻿using QL_LopHocPhan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace QL_LopHocPhan.Controllers
+{
+    public class SinhVienController : Controller
+    {
+
+        DatabaseDataContext db = new DatabaseDataContext();
+        // GET: SinhVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Lấy ra danh sách sinh viên
+        public string GetList()
+        {
+            var dssv = db.tbl_SinhViens;
+            return JsonConvert.SerializeObject(dssv);
+        }
+    }
+}
