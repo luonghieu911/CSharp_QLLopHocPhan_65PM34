f40f2b353735ee65eddf85446bc04cf93c53359f
luonghieu911 <hieulx1st@gmail.com>
2023-05-22 09:12:16 +0700

    Thêm Result_ett, EnumErrCode

---
+++ QL_LopHocPhan/QL_LopHocPhan/Models/EnumErrCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QL_LopHocPhan.Models
+{
+    public enum EnumErrCode
+    {
+        Error,
+        Success,
+        Fail
+    }
+}
